80b15e5603c34d0c00c0ae44d251983a8e9343e5
searchsakib <searchsakib@gmail.com>
2024-05-21 19:28:02 +0600

    prettier installed

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -22,5 +22,5 @@ export default tseslint.config(
   },
   {
     ignores: ['**/node_modules/', '**/dist/'],
-  }
+  },
 );
---
+++ src/modules/products/products.interface.ts
@@ -0,0 +1,8 @@
+export type TProduct = {
+  name: string;
+  description: string;
+  price: number;
+  category: string;
+  tags: string[];
+  // variantes:
+};
